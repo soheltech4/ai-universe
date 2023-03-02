4a5fb8315ad4c2065c0ae953732f9ec2397b6663
sohelranagtc <sohelranagtc@gmail.com>
2023-03-02 21:10:24 +0600

    fetch url

--- ai-universe-hub-js/ai-universe-hub.js
+++ ai-universe-hub-js/ai-universe-hub.js
@@ -1,1 +1,8 @@
-console.log('Yes Its Connected')
+const LoadData = () => {
+    const url = `https://openapi.programming-hero.com/api/ai/tools`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data.data))
+}
+
+LoadData()
